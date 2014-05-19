09fe8f34183b33090fffa3974d1f0e94d6867d5b
bsimon2 <>
2014-05-19 21:10:07 +0200

    download seems to work now

--- download.php
+++ download.php
@@ -25,22 +25,22 @@
     $name = "results.zip";    
 
     clearstatcache();
-//    header('Content-Type: application/zip');
-//    header('Content-disposition: attachment; filename=results.zip');
-//    header('Content-Length: '.filesize($name));
-//    readfile($name);
-//    unlink($name);
-    
-    header("Pragma: public");
-    header("Expires: 0");
-    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-    header("Cache-Control: public");
-    header("Content-Description: File Transfer");
-    header("Content-type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=\"".$name."\"");
-    header("Content-Transfer-Encoding: binary");
+    header("Content-Type: application/zip");
+    header("Content-disposition: attachment; filename=$name");
     header("Content-Length: ".filesize($name));
-    ob_end_flush();
     readfile($name);
     unlink($name);
+    
+//    header("Pragma: public");
+//    header("Expires: 0");
+//    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+//    header("Cache-Control: public");
+//    header("Content-Description: File Transfer");
+//    header("Content-type: application/octet-stream");
+//    header("Content-Disposition: attachment; filename=\"".$name."\"");
+//    header("Content-Transfer-Encoding: binary");
+//    header("Content-Length: ".filesize($name));
+//    ob_end_flush();
+//    readfile($name);
+//    unlink($name);
 ?>
